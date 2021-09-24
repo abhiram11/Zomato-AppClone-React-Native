772b72843a871ab48b5656bd4245160baedd10cb
Abhiram Satpute <abhiramsatpute@gmail.com>
2021-09-24 10:01:44 +0530

    added categories, and see more

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
+import FoodCategories from "../components/FoodCategories";
 import ProfileLocation from "../components/ProfileLocation";
 import SearchHeader from "../components/SearchHeader";
 
 export default function Home() {
   return (
-    <View>
+    <View style={{ margin: 6 }}>
       <ProfileLocation />
       <SearchHeader />
+      <FoodCategories />
     </View>
   );
 }
