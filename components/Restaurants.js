416bdba193a9c29823b05a0c9921a39eb6c8d154
Abhiram Satpute <abhiramsatpute@gmail.com>
2021-09-24 11:37:15 +0530

    restr. extra details added as well with styling, semi-final touching required

--- components/Restaurants.js
+++ components/Restaurants.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
+import Entypo from "react-native-vector-icons/Entypo";
 
 export default function Restaurants() {
   return (
@@ -9,6 +10,10 @@ export default function Restaurants() {
       </Text>
       {/* reusable component */}
       <RestaurantInfo />
+      <RestaurantInfo />
+      <RestaurantInfo />
+      <RestaurantInfo />
+      <RestaurantInfo />
     </View>
   );
 }
@@ -16,12 +21,84 @@ export default function Restaurants() {
 const RestaurantInfo = () => (
   // TODO change border radius to box shadow
   <View style={{ margin: 10, borderWidth: 1, borderRadius: 18 }}>
-    <Image
-      source={{
-        uri: "https://www.manjulaskitchen.com/wp-content/uploads/everyday-lunch-menu-1024x576.jpg",
-      }}
-      style={{ width: "100%", height: 200, borderRadius: 18 }}
-    />
+    <View>
+      <Image
+        source={{
+          uri: "https://www.manjulaskitchen.com/wp-content/uploads/everyday-lunch-menu-1024x576.jpg",
+        }}
+        style={{
+          width: "100%",
+          height: 200,
+          borderTopLeftRadius: 18,
+          borderTopRightRadius: 18,
+        }}
+      />
+      {/* EXTRA COMPONENTS on IMAGE */}
+      <Text
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 10,
+          paddingHorizontal: 4,
+          fontSize: 12,
+          fontWeight: "bold",
+          color: "white",
+          backgroundColor: "#7b797b",
+          opacity: 0.6,
+          borderRadius: 4,
+        }}
+      >
+        Promoted
+      </Text>
+      <Entypo
+        style={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          color: "gray",
+          backgroundColor: "white",
+          padding: 8,
+          borderRadius: 100,
+        }}
+        name="bookmark"
+        size={20}
+      />
+      <Text
+        style={{
+          position: "absolute",
+          right: 10,
+          bottom: 10,
+          backgroundColor: "white",
+          paddingVertical: 2,
+          paddingHorizontal: 4,
+          borderRadius: 4,
+          fontSize: 12,
+          fontWeight: "bold",
+          color: "#7b797b",
+        }}
+      >
+        24 mins
+      </Text>
+      <Text
+        style={{
+          position: "absolute",
+          bottom: 10,
+          color: "white",
+          backgroundColor: "crimson",
+          fontWeight: "bold",
+          paddingVertical: 2,
+          paddingHorizontal: 6,
+          borderTopRightRadius: 4,
+          borderBottomRightRadius: 4,
+        }}
+      >
+        PRO extra 25% OFF
+      </Text>
+
+      {/* EXTRA COMPONENTS END HERE */}
+    </View>
+
+    {/* UNDER IMAGE INFO  */}
     <View>
       <View
         style={{
--- screens/Home.js
+++ screens/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
       <View style={{ margin: 6 }}>
         <ProfileLocation />
         <SearchHeader />
-        {/* <FoodCategories /> */}
+        <FoodCategories />
         <Restaurants />
       </View>
     </ScrollView>
