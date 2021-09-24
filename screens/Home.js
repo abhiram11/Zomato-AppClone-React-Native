ba0906ca046862a18cfaf52c6c3ac492eaff2adf
Abhiram Satpute <abhiramsatpute@gmail.com>
2021-09-24 15:22:21 +0530

    footer done, home screen complete as of now, moving on next restaurant screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { View, Text, ScrollView } from "react-native";
 import FoodCategories from "../components/FoodCategories";
+import Footer from "../components/Footer";
 import ProfileLocation from "../components/ProfileLocation";
 import Restaurants from "../components/Restaurants";
 import SearchHeader from "../components/SearchHeader";
 
 export default function Home() {
   return (
-    <ScrollView vertical showsVerticalScrollIndicator={false}>
-      <View style={{ margin: 6 }}>
+    <View style={{ margin: 6 }}>
+      <ScrollView vertical showsVerticalScrollIndicator={false}>
         <ProfileLocation />
         <SearchHeader />
         <FoodCategories />
         <Restaurants />
-      </View>
-    </ScrollView>
+      </ScrollView>
+      <Footer />
+    </View>
   );
 }
